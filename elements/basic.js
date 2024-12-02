b499f65978c5416d9dddac823f2855feeb65e73a
Martin <bacovsmar@seznam.cz>
2024-12-02 20:37:10 +0100

    Test

--- elements/basic.js
+++ elements/basic.js
@@ -1,12 +1,23 @@
 import { enemyRadius, playerRadius } from "constants";
 
+let lastTime = Date.now();
+let elapsedTime = 0;
+let timePeriod = 5000.0;
+let isGrowing = true;
+
 function linearGradient(canvas, ctx) {
+    elapsedTime += ((isGrowing) ? 1.0 : (-1.0)) * (Date.now() - lastTime) / timePeriod;
+    lastTime = Date.now();
+
     const grd = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
     grd.addColorStop(0, 'red');
-    grd.addColorStop(0.5, 'darkgreen');
-    grd.addColorStop(1, 'black');
+    grd.addColorStop(elapsedTime, 'darkgreen');
+    grd.addColorStop(1, '#000');
     ctx.fillStyle = grd;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    if (elapsedTime >= 0.98) {isGrowing = false; elapsedTime = 0.95;}
+    if (elapsedTime <= 0.05) {isGrowing = true; elapsedTime = 0.06;}
 }
 
 function clear(canvas, ctx) {
@@ -28,11 +39,13 @@ function drawCircle(ctx, sx, sy, radius, width, color) {
 }
 
 export function drawPlayer(ctx, sx, sy) {
-    drawCircle(ctx, sx, sy, playerRadius, 3, "lightgreen");
+    drawCircle(ctx, sx - 2, sy - 2, playerRadius, 5, "#0A0");
+    drawCircle(ctx, sx, sy, playerRadius, 3, "#0E0");
 }
 
 export function drawEnemy(ctx, sx, sy) {
-    drawCircle(ctx, sx, sy, enemyRadius,2, 'red');
+    drawCircle(ctx, sx + 2, sy + 2, enemyRadius,5, '#A00');
+    drawCircle(ctx, sx, sy, enemyRadius,2, '#D00');
 }
 
 export {drawCircle, linearGradient, clear};
